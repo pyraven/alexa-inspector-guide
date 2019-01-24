3db05a7d1cc081a77810269202fe07606c96442b
w1ld-rav3n <austin.hillman@live.com>
2019-01-23 20:12:03 -0800

    Init Commit

---
+++ lambda_function.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+
+# This is a simple Hello World Alexa Skill, built using
+# the implementation of handler classes approach in skill builder.
+import logging
+import boto3
+import datetime
+
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+from ask_sdk_core.utils import is_request_type, is_intent_name
+from ask_sdk_core.handler_input import HandlerInput
+
+from ask_sdk_model.ui import SimpleCard
+from ask_sdk_model import Response
+
+sb = SkillBuilder()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class LaunchRequestHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_request_type("LaunchRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "Welcome to the Alexa Skills Kit, you can say hello!"
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Hello World", speech_text)).set_should_end_session(
+            False)
+        return handler_input.response_builder.response
+
+
+class HelloWorldIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("HelloWorldIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "Hello AI Gurus!"
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Hello World", speech_text)).set_should_end_session(
+            True)
+        return handler_input.response_builder.response
+
+
+class HelpIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("AMAZON.HelpIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "You can say hello to me!"
+
+        handler_input.response_builder.speak(speech_text).ask(
+            speech_text).set_card(SimpleCard(
+                "Hello World", speech_text))
+        return handler_input.response_builder.response
+
+
+class CancelOrStopIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return (is_intent_name("AMAZON.CancelIntent")(handler_input) or
+                is_intent_name("AMAZON.StopIntent")(handler_input))
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = "Goodbye!"
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Hello World", speech_text))
+        return handler_input.response_builder.response
+
+
+class FallbackIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("AMAZON.FallbackIntent")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speech_text = (
+            "The Hello World skill can't help you with that.  "
+            "You can say hello!!")
+        reprompt = "You can say hello!!"
+        handler_input.response_builder.speak(speech_text).ask(reprompt)
+        return handler_input.response_builder.response
+
+
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        return handler_input.response_builder.response
+
+
+class CatchAllExceptionHandler(AbstractExceptionHandler):
+    def can_handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> bool
+        return True
+
+    def handle(self, handler_input, exception):
+        # type: (HandlerInput, Exception) -> Response
+        logger.error(exception, exc_info=True)
+
+        speech = "Sorry, there was some problem. Please try again!!"
+        handler_input.response_builder.speak(speech).ask(speech)
+
+        return handler_input.response_builder.response
+
+
+##################################
+###       Custom Skill        ####
+###       Scan Servers        ####
+##################################
+
+class ScanServerIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("ScanServerIntent")(handler_input)
+
+    def handle(self, handler_input):
+         # type: (HandlerInput) -> Response
+        slots = handler_input.request_envelope.request.intent.slots
+        server = slots["server"].value
+        inspector_scan_list = []
+        client = boto3.client('inspector')
+        template_list = [i for i in client.list_assessment_templates()['assessmentTemplateArns']]
+        for template in template_list:
+            assessment = client.list_tags_for_resource(resourceArn=template)['tags']
+            for tag in assessment:
+                if tag['key'] == 'Name':
+                    if tag['value'].lower() == server.lower():
+                        inspector_scan_list.append(template)
+        if len(inspector_scan_list) > 0:
+            for scan in inspector_scan_list:
+                try:
+                    scan_name = datetime.datetime.now().strftime(f"%Y-%m-%d %H:%M:%S_{server}_Vuln_Assessment")
+                    response = client.start_assessment_run(
+                                    assessmentTemplateArn=scan,
+                                    assessmentRunName=scan_name)
+                    speech_text = (f"Scanning {server}.")
+                except Exception as e:
+                    print(f"Error: {e}")
+                    speech_text = ("There was a problem with the scan request.")
+        else:
+            speech_text = ("Unable to locate server or servers to scan.")
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Hello World", speech_text)).set_should_end_session(True)
+        return handler_input.response_builder.response
+
+
+##################################
+###       Skillbuilder        ####
+##################################
+
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(HelloWorldIntentHandler())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(CancelOrStopIntentHandler())
+sb.add_request_handler(FallbackIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+sb.add_exception_handler(CatchAllExceptionHandler())
+
+# custom
+sb.add_request_handler(ScanServerIntentHandler())
+
+lambda_handler = sb.lambda_handler()
